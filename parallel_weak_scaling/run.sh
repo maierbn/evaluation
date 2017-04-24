73c1f8230962988b5031c3d76bb2257273512ca4
Benjamin Maier <maierbn@gmail.com>
2017-04-24 17:55:22 +0200

    Update run.sh

--- parallel_weak_scaling/run.sh
+++ parallel_weak_scaling/run.sh
@@ -3,13 +3,13 @@
 date
 module add shared openmpi
 
-echo "OPENCMISS_REL_DIR=$OPENCMISS_REL_DIR, OPENCMISS_REL_DIR=$OPENCMISS_REL_DIR"
+echo "OPENCMISS_REL_DIR=$OPENCMISS_REL_DIR, OPENCMISS_INPUT_DIR=$OPENCMISS_INPUT_DIR"
 
-. /etc/profile.d/modules.sh
 . ~/.bashrc
 
 function check_exit()
 {
+   return # check disabled!
    while true; do
       HOUR=$(date +%H)
       echo "Current time: $(date)"
@@ -26,26 +26,36 @@ function check_exit()
 
 echo "weak scaling"
 
-#while true; do
+# 1 2 1 = GMRES, no precond.
+# 1 3 6 = CG, ILU
+
+while true; do
   check_exit
-  mpiexec -n 1 $OPENCMISS_REL_DIR/cuboid $OPENCMISS_INPUT_DIR 3 8 1 1
+  mpiexec -n 1 $OPENCMISS_REL_DIR/cuboid $OPENCMISS_INPUT_DIR 3 8 1 1 1     1 2 1
+  mpiexec -n 1 $OPENCMISS_REL_DIR/cuboid $OPENCMISS_INPUT_DIR 3 8 1 1 1     1 3 6
   check_exit
-  mpiexec -n 2 $OPENCMISS_REL_DIR/cuboid $OPENCMISS_INPUT_DIR 3 8 2 1
+  mpiexec -n 2 $OPENCMISS_REL_DIR/cuboid $OPENCMISS_INPUT_DIR 3 8 2 1 1     1 2 1
+  mpiexec -n 2 $OPENCMISS_REL_DIR/cuboid $OPENCMISS_INPUT_DIR 3 8 2 1 1     1 3 6
   check_exit
-  mpiexec -n 4 $OPENCMISS_REL_DIR/cuboid $OPENCMISS_INPUT_DIR 3 16 2 1
+  mpiexec -n 4 $OPENCMISS_REL_DIR/cuboid $OPENCMISS_INPUT_DIR 3 16 2 1 1    1 2 1
+  mpiexec -n 4 $OPENCMISS_REL_DIR/cuboid $OPENCMISS_INPUT_DIR 3 16 2 1 1    1 3 6
   check_exit
-  mpiexec -n 8 $OPENCMISS_REL_DIR/cuboid $OPENCMISS_INPUT_DIR 6 16 2 1
+  mpiexec -n 8 $OPENCMISS_REL_DIR/cuboid $OPENCMISS_INPUT_DIR 6 16 2 1 1    1 2 1
+  mpiexec -n 8 $OPENCMISS_REL_DIR/cuboid $OPENCMISS_INPUT_DIR 6 16 2 1 1    1 3 6
   check_exit
-  mpiexec -n 12 $OPENCMISS_REL_DIR/cuboid $OPENCMISS_INPUT_DIR 6 16 3 1
+  mpiexec -n 12 $OPENCMISS_REL_DIR/cuboid $OPENCMISS_INPUT_DIR 6 16 3 1 1   1 2 1
+  mpiexec -n 12 $OPENCMISS_REL_DIR/cuboid $OPENCMISS_INPUT_DIR 6 16 3 1 1   1 3 6
   check_exit
-  mpiexec -n 16 $OPENCMISS_REL_DIR/cuboid $OPENCMISS_INPUT_DIR 6 16 4 1
+  mpiexec -n 16 $OPENCMISS_REL_DIR/cuboid $OPENCMISS_INPUT_DIR 6 16 4 1 1   1 2 1
+  mpiexec -n 16 $OPENCMISS_REL_DIR/cuboid $OPENCMISS_INPUT_DIR 6 16 4 1 1   1 3 6
   check_exit
-  mpiexec -n 24 $OPENCMISS_REL_DIR/cuboid $OPENCMISS_INPUT_DIR 6 24 4 1
+  mpiexec -n 24 $OPENCMISS_REL_DIR/cuboid $OPENCMISS_INPUT_DIR 6 24 4 1 1   1 2 1
+  mpiexec -n 24 $OPENCMISS_REL_DIR/cuboid $OPENCMISS_INPUT_DIR 6 24 4 1 1   1 3 6
   check_exit
-  mpiexec -n 32 $OPENCMISS_REL_DIR/cuboid $OPENCMISS_INPUT_DIR 6 32 4 1
+  mpiexec -n 32 $OPENCMISS_REL_DIR/cuboid $OPENCMISS_INPUT_DIR 6 32 4 1 1   1 2 1
+  mpiexec -n 32 $OPENCMISS_REL_DIR/cuboid $OPENCMISS_INPUT_DIR 6 32 4 1 1   1 3 6
   check_exit
-  mpiexec -n 64 $OPENCMISS_REL_DIR/cuboid $OPENCMISS_INPUT_DIR 12 32 4 1
+  mpiexec -n 64 $OPENCMISS_REL_DIR/cuboid $OPENCMISS_INPUT_DIR 12 32 4 1 1  1 2 1
+  mpiexec -n 64 $OPENCMISS_REL_DIR/cuboid $OPENCMISS_INPUT_DIR 12 32 4 1 1  1 3 6
   check_exit
-  mpiexec -n 96 $OPENCMISS_REL_DIR/cuboid $OPENCMISS_INPUT_DIR 12 32 6 1
-  check_exit
-#done
+done
